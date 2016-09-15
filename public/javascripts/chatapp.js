b42242defd63889a929e13e38942b49881390ac2
Jonny Pickard <jonathandpickard@gmail.com>
2016-09-15 17:59:19 +0100

    Further formatting on timestamp

--- public/javascripts/chatapp.js
+++ public/javascripts/chatapp.js
@@ -6,8 +6,19 @@ app.controller('chatController', function($scope) {
   $scope.addChat = function(){
     $scope.chatList.push({
       text: $scope.chat,
-      time: Date.now(),
+      time: getDate()
     });
   };
 
 });
+
+function getDate() {
+  var d = new Date();
+  var curr_date = d.getDate();
+  var curr_month = d.getMonth() + 1; //Months are zero based
+  var curr_year = d.getFullYear();
+  var hours = d.getHours();
+  var minutes = d.getMinutes();
+  var date = (hours + ":" + minutes + " - " + curr_date + "-" + curr_month + "-" + curr_year);
+  return date;
+}
