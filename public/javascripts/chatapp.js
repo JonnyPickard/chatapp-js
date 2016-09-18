96ac2ee9737af7df30b0f6a3d6d0421393e50c8d
Jonny Pickard <jonathandpickard@gmail.com>
2016-09-18 22:01:30 +0100

    Add simple user login

--- public/javascripts/chatapp.js
+++ public/javascripts/chatapp.js
@@ -1,13 +1,37 @@
 var app = angular.module('chatapp', []);
 
 app.controller('chatController', function($scope) {
+
+  var user = {
+    name: ""
+  };
+
   $scope.chatList = [];
+
+  $scope.getUsername = function() {
+    return user.name;
+  };
+
+  $scope.isLoggedIn = function(){
+    if (user.name === "") {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  $scope.logIn = function(){
+    user.name = $scope.username;
+  };
 
   $scope.addChat = function(){
     $scope.chatList.push({
       text: $scope.chat,
-      time: getDate()
+      time: getDate(),
+      user: user.name,
+      created_at: Date.now()
     });
+    $scope.chat = "";
   };
 
 });
--- spec/chatapp_spec.js
+++ spec/chatapp_spec.js
@@ -1,12 +1,18 @@
 describe('chatapp home page chat list', function(){
   it('should add a chat', function(){
-    browser.get('http://localhost:3000/');
+    browser.get('http://localhost:3000');
+
+    //login
+    element(by.model('username')).sendKeys('testUser');
+    element(by.css('[value="Login"]')).click();
 
     element(by.css('.chatInput')).sendKeys('First chatapp test');
     element(by.css('[value="Chat"]')).click();
 
-    var chatFeed = element(by.css('.chatFeed')).getText();
+    var chatText = element(by.id('chatText')).getText();
+    var chatUser = element(by.id('chatUser')).getText();
 
-    expect(chatFeed).toEqual('First chatapp test');
+    expect(chatText).toEqual('First chatapp test');
+    expect(chatUser).toEqual('by: testUser');
   });
 });
